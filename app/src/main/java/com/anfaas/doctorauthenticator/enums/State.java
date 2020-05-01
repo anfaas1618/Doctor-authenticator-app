ccdbe2fb196f28b7002c90c22e59ae0079850e98
anfaas1618 <ishad0911@gmail.com>
2020-05-01 23:01:47 +0530

    added back enum

---
+++ app/src/main/java/com/anfaas/doctorauthenticator/enums/State.java
@@ -0,0 +1,43 @@
+package com.anfaas.doctorauthenticator.enums;
+
+import org.apache.commons.text.WordUtils;
+
+public enum State {
+    ANDHRA_PRADESH,
+    ARUNACHAL_PRADESH,
+    ASSAM,
+    BIHAR,
+    DELHI,
+    GOA,
+    GUJARAT,
+    HARYANA,
+    HIMACHAL_PRADESH,
+    JAMMU_n_KASHMIR,
+    JHARKHAND,
+    KARNATAKA,
+    MADHYA_PRADESH,
+    MAHARASHTRA,
+    MANIPUR,
+    MIZORAM,
+    NAGALAND,
+    ORISSA,
+    PUNJAB,
+    RAJASTHAN,
+    SIKKIM,
+    TAMIL_NADU,
+    TELANGANA,
+    KERALA,
+    TRIPURA,
+    UTTARAKHAND,
+    UTTAR_PRADESH,
+    WEST_BENGAL
+    ;
+
+    public String getCouncilName() {
+        return  fixCase(name().replace("KERALA","Travancore Cochin").replace("_n_"," & ").replace("_"," ")).trim() + " Medical Council";
+    }
+
+    private static String fixCase(String s){
+        return WordUtils.capitalizeFully(s);
+    }
+}
